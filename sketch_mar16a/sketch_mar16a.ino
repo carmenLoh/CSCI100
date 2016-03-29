9382d02adbf28cf35c591702b1fe70b0cc8d906f
carmenLoh <carmen.430520@gmail.com>
2016-03-29 14:51:35 -0400

    Update sketch_mar16a.ino

--- sketch_mar16a/sketch_mar16a.ino
+++ sketch_mar16a/sketch_mar16a.ino
@@ -1,22 +1,43 @@
-//carmen loh
-//in this project i used a 3 pin switch to replace the push button.
-int led_pin = 7;
-int switch_pin = 12;
-void setup() {
- pinMode(led_pin, OUTPUT);
- pinMode(switch_pin, INPUT_PULLUP);
+int wallLeft = 12;
+int wallFront = 6;
+int wallRight = 9;
+int goLeft = 1;
+int goStraight = 0;
+int goRight = 2;
+int turnAround = 3;
+
+void setup()
+{
+  pinMode(wallLeft, INPUT_PULLUP);
+  pinMode(wallFront, INPUT_PULLUP);
+  pinMode(wallRight, INPUT_PULLUP);
+  pinMode(goLeft, OUTPUT);
+  pinMode(goStraight, OUTPUT);
+  pinMode(goRight, OUTPUT);
+  pinMode(turnAround, OUTPUT);
 }
 
-void loop() {
-  int switch_state = digitalRead(switch_pin);
-  if (switch_state)
+void loop()
+{
+  digitalWrite(turnAround, LOW);
+  digitalWrite(goRight, LOW);
+  digitalWrite(goStraight, LOW);
+  digitalWrite(goLeft, LOW);
+  if (digitalRead(wallRight) == 1) //on wall on right
   {
-    delay(1000);
-    digitalWrite (led_pin, HIGH);
-    }
+    digitalWrite(goRight, HIGH); //go right LED is on
+  }
+  else if (digitalRead(wallFront) == 1)
+  {
+    digitalWrite(goStraight, HIGH);
+  }
+  else if (digitalRead(wallLeft) == 1)
+  {
+    digitalWrite(goLeft, HIGH);
+  }
   else
   {
-    delay(2000);
-    digitalWrite (led_pin, LOW);
+    digitalWrite(turnAround, HIGH);
   }
+
 }
